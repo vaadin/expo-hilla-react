82d5bbbf182bf5e5d818938964364ec5f8f71266
Marcus Hellberg <marcus@vaadin.com>
2023-06-17 18:30:53 -0700

    Remove unused import

--- frontend/views/GridView.tsx
+++ frontend/views/GridView.tsx
@@ -2,23 +2,22 @@ import { Grid } from '@hilla/react-components/Grid.js';
 import { useEffect, useState } from 'react';
 import Person from 'Frontend/generated/com/example/application/data/entity/Person';
 import { PersonEndpoint } from 'Frontend/generated/endpoints';
-import { GridColumn } from '@hilla/react-components/GridColumn.js';
 import { GridSortColumn } from '@hilla/react-components/GridSortColumn';
 
 export function GridView() {
   const [people, setPeople] = useState<Person[]>([]);
 
   useEffect(() => {
-    PersonEndpoint.findAll().then(setPeople)
+    PersonEndpoint.findAll().then(setPeople);
   }, []);
 
 
   return (
-    <Grid items={people} className="m-m h-full">
-      <GridSortColumn path="firstName"/>
-      <GridSortColumn path="lastName"/>
-      <GridSortColumn path="email" autoWidth/>
-      <GridSortColumn path="dateOfBirth"/>
+    <Grid items={people} className='m-m h-full'>
+      <GridSortColumn path='firstName' />
+      <GridSortColumn path='lastName' />
+      <GridSortColumn path='email' autoWidth />
+      <GridSortColumn path='dateOfBirth' />
     </Grid>
   );
 }
